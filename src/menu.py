efd1f4d3009596f85b8acc8f808bc5995cd917e4
DevOps Don <devops-don@proton.me>
2024-09-13 09:39:09 -0400

    feat: added a help description option

--- src/menu.py
+++ src/menu.py
@@ -9,6 +9,18 @@ from src.utils import clear_screen
 def default():
     print("Not a valid option, please try again.")
 
+def help_option():
+    print("""
+          - Select from the available options to perform an action.\n
+          - You can choose to either select individual parts to add to\n
+          your shopping cart or build a custom PC.\n
+          - You can also check the compatibility of parts for a custom PC\n
+          build.\n
+          - Please open an issue ticket on our github page for bugs or\n
+          new features.\n
+          - Please review our README before contacting support. Thank you!
+          """)
+
 def exit_program():
     clear_screen()
     print("Thank you and Goodbye!")
@@ -21,7 +33,7 @@ def display_menu(user_name, budget):
     print("--------------------------------------------------------------------------")
     print("Options Placeholder")
     print("--------------------------------------------------------------------------")
-    print("E) Exit")
+    print("H) Help    E) Exit")
 
 def main_menu(user_name, budget, inventory_data):
     while True:
@@ -30,7 +42,8 @@ def main_menu(user_name, budget, inventory_data):
 
         # Dictionary of available command options
         menu_dict: dict = {
-            "e": exit_program,   
+            "h": help_option,
+            "e": exit_program,
         }
 
         clear_screen()
