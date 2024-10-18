4a2c6132aa01f79aa9a0533093aea417b5e8248e
DevOps Don <devops-don@proton.me>
2024-10-18 11:19:10 -0400

    C0301: Line too long (101/100) (line-too-long)
    storefront.py:40:0: C0301: Line too long (101/100) (line-too-long)

--- storefront.py
+++ storefront.py
@@ -37,7 +37,8 @@ def main():
 
     if len(sys.argv) != 2:
         print("\nUsage: python3 storefront.py inventory.json")
-        print("Or you can make storefront.py an exicutable and run ./storefront.py inventory.json\n")
+        print("Or you can make storefront.py an exicutable and run \
+              ./storefront.py inventory.json\n")
         sys.exit(1)
     
     inventory_file = sys.argv[1]
